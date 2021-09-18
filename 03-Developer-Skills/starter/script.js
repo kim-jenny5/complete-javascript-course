da902712dd150be8f1879601aa5a5b340407cb40
Jenny Kim <kimj1905@gmail.com>
2021-09-18 16:11:50 -0400

    finishes with 03 challenges

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const arr = [17, 21, 23];
+const arr2 = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+  const dots = `...`;
+  let pushArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    pushArr.push(`${arr[i]}°C in ${i + 1} ${i > 0 ? "days" : "day"}`);
+  }
+  console.log(`${dots} ` + pushArr.join(` ${dots} `) + ` ${dots}`);
+}
+
+printForecast(arr);
+printForecast(arr2);
