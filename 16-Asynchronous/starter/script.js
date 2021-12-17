28cd17845971fa6d3c2f4444c7c4af24a6b92b76
Jenny Kim <kimj1905@gmail.com>
2021-12-17 18:48:12 -0500

    completed coding challenge 1

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,4 +3,52 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+// NOTE: Coding Challenge #1
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const whereAmI = (lat, lng) => {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(resp => {
+      if (!resp.ok) throw new Error('Failed to retrieve data from server.');
+      return resp.json();
+    })
+    .then(data => {
+      console.log(`You are in ${data.state}, ${data.country}.`);
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(resp => {
+      if (!resp.ok) throw new Error('Country not found.');
+      return resp.json();
+    })
+    .then(datas => {
+      if (datas.length > 1) {
+        for (const data of datas) {
+          return renderCountry(data);
+        }
+      } else return renderCountry(datas[0]);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
