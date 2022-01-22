179badd8390c56210dedc5f2bf596a167bfffadb
Jenny Kim <kimj1905@gmail.com>
2022-01-21 19:13:00 -0500

    finished coding challenge #2 midway

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,54 +1,134 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 // NOTE: Coding Challenge #1
-const renderCountry = function (data, className = '') {
-  const html = `
+const renderCountry = function (data, className = "") {
+	const html = `
     <article class="country ${className}">
       <img class="country__img" src="${data.flag}" />
       <div class="country__data">
         <h3 class="country__name">${data.name}</h3>
         <h4 class="country__region">${data.region}</h4>
         <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)} people</p>
+					+data.population / 1000000
+				).toFixed(1)} people</p>
         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
         <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
       </div>
     </article>
     `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
+	countriesContainer.insertAdjacentHTML("beforeend", html);
+	countriesContainer.style.opacity = 1;
 };
 
-const whereAmI = (lat, lng) => {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then(resp => {
-      if (!resp.ok) throw new Error('Failed to retrieve data from server.');
-      return resp.json();
-    })
-    .then(data => {
-      console.log(`You are in ${data.state}, ${data.country}.`);
-      return fetch(`https://restcountries.com/v2/name/${data.country}`);
-    })
-    .then(resp => {
-      if (!resp.ok) throw new Error('Country not found.');
-      return resp.json();
-    })
-    .then(datas => {
-      if (datas.length > 1) {
-        for (const data of datas) {
-          return renderCountry(data);
-        }
-      } else return renderCountry(datas[0]);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-};
+// const whereAmI = (lat, lng) => {
+// 	fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+// 		.then((resp) => {
+// 			if (!resp.ok) throw new Error("Failed to retrieve data from server.");
+// 			return resp.json();
+// 		})
+// 		.then((data) => {
+// 			console.log(`You are in ${data.state}, ${data.country}.`);
+// 			return fetch(`https://restcountries.com/v2/name/${data.country}`);
+// 		})
+// 		.then((resp) => {
+// 			if (!resp.ok) throw new Error("Country not found.");
+// 			return resp.json();
+// 		})
+// 		.then((datas) => {
+// 			if (datas.length > 1) {
+// 				for (const data of datas) {
+// 					return renderCountry(data);
+// 				}
+// 			} else return renderCountry(datas[0]);
+// 		})
+// 		.catch((error) => {
+// 			console.log(error);
+// 		});
+// };
 
-whereAmI(52.508, 13.381);
+// whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
+
+// Asynchronous JavaScript
+// const lotteryPromise = new Promise((resolve, reject) => {});
+// console.log("Getting position");
+
+const getPosition = () => {
+	return new Promise(function (resolve, reject) {
+		// navigator.geolocation.getCurrentPosition(
+		// 	(position) => resolve(position),
+		// 	(err) => reject(err)
+		// );
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
+};
+
+// getPosition().then((pos) => console.log(pos));
+
+const whereAmI = () => {
+	getPosition()
+		.then((pos) => {
+			const { latitude: lat, longitude: lng } = pos.coords;
+			return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+		})
+		.then((resp) => {
+			if (!resp.ok) throw new Error("Failed to retrieve data from server.");
+			return resp.json();
+		})
+		.then((data) => {
+			console.log(`You are in ${data.state}, ${data.country}.`);
+			return fetch(`https://restcountries.com/v2/name/${data.country}`);
+		})
+		.then((resp) => {
+			if (!resp.ok) throw new Error("Country not found.");
+			return resp.json();
+		})
+		.then((datas) => {
+			if (datas.length > 1) {
+				for (const data of datas) {
+					return renderCountry(data);
+				}
+			} else return renderCountry(datas[0]);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
+// whereAmI();
+
+btn.addEventListener("click", whereAmI);
+
+const createImage = (imgPath) => {
+	return new Promise((resolve, reject) => {
+		const img = document.createElement("img");
+		img.src = imgPath;
+		// .setAttribute("src", imgPath)
+		// .setAttribute("class", "images");
+		img.addEventListener("load", () => {
+			img.setAttribute("class", "images");
+
+			document.querySelector(".images").append(img);
+			resolve(img);
+		});
+		img.addEventListener("error", () => {
+			reject(new Error("Trouble loading image."));
+		});
+	})
+		.then(() => {
+			wait(2);
+		})
+		.catch();
+};
+
+// for (let i = 1; i <= 3; i++) {
+// 	createImage(`./img/img-${i}.jpg`);
+// }
+
+// createImage("./img/img-1.jpg");
+// createImage("./img/img-2.jpg");
+// createImage("./img/img-3.jpg");
